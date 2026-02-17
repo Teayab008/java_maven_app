b4ffe8320f4ea83d5dcae453d7403905b60bb475
Ahmad_abdullah <fa22-bee-043@cuilahore.edu.pk>
2026-02-17 12:38:03 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,19 @@
-def buildApp(){
-  echo 'building the appliation'
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
 }
-def testApp(){
-  echo 'testing the appliation'
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
+    }
 }
-def deployApp(){
-  echo 'deploying the appliation'
-  echo "deploying version ${params.version}"
+
+def deployApp() {
+    echo 'deploying the application...'
 }
-return this 
+
+return this
